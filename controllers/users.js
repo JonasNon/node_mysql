34c70da6df84a34fcf2191a5f238b88bbcf7098b
JonasNon <officialjonasn@gmail.com>
2024-06-22 19:42:29 -0500

    finised some things

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,22 @@
 const mysql = require('mysql')
 const pool = require('../sql/connection')
+const body = require('body-parser')
 const { handleSQLError } = require('../sql/error')
 
 const getAllUsers = (req, res) => {
   // SELECT ALL USERS
-  pool.query("SELECT * FROM users", (err, rows) => {
+  pool.query("SELECT * FROM USERS INNER JOIN USERSADDRESS ON USERS.ID = USERSADDRESS.ID JOIN USERSCONTACT ON USERS.ID = USERSCONTACT.ID", (err, rows) => {
     if (err) return handleSQLError(res, err)
     return res.json(rows);
   })
 }
-
+//  INNER JOIN USERSCONTACT WHERE USERS.ID = USERSCONTACT.ID
 const getUserById = (req, res) => {
   // SELECT USERS WHERE ID = <REQ PARAMS ID>
-  let sql = "QUERY GOES HERE"
+  let sql = "SELECT * FROM USERS WHERE ID = ? "
+  let id = req.params.id
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, [id])
 
   pool.query(sql, (err, rows) => {
     if (err) return handleSQLError(res, err)
@@ -24,21 +26,40 @@ const getUserById = (req, res) => {
 
 const createUser = (req, res) => {
   // INSERT INTO USERS FIRST AND LAST NAME 
-  let sql = "QUERY GOES HERE"
+  let sql = "INSERT INTO USERS (FIRST_NAME, LAST_NAME) VALUES (?, ?)"
+  console.log(req.query[Object.keys(req.query)[0]], req.query[Object.keys(req.query)[1]])
+  console.log(req.body.first_name, req.body.last_name)
+
+  let sqlAddress = "INSERT INTO USERSADDRESS (FIRST_NAME, LAST_NAME) VALUES (?, ?)"
+
+
+  // ??????????????????????????????????????????????????????????
+
+
+
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, [req.body.first_name, req.body.last_name])
+
+  sqlAddress = mysql.format(sqlAddress, [req.body.first_name, req.body.last_name])
+
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
     return res.json({ newId: results.insertId });
+    
   })
+  // pool.query(sqlAddress, (err, results) => {
+  //   if (err) return handleSQLError(res, err)
+  //   return res.json({ newId: results.insertId });
+  // })
 }
 
 const updateUserById = (req, res) => {
   // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
-  let sql = ""
+  let sql = "UPDATE USERS SET FIRST_NAME = ?, LAST_NAME = ? WHERE ID = ?"
+  let id = req.params.id
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, [req.body.first_name, req.body.last_name, id])
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
@@ -48,9 +69,10 @@ const updateUserById = (req, res) => {
 
 const deleteUserByFirstName = (req, res) => {
   // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
-  let sql = ""
+  let sql = "DELETE FROM USERS WHERE FIRST_NAME = ?"
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  // console.log(req.params.first_name)
+  sql = mysql.format(sql, [req.params.first_name])
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
--- sql/connection.js
+++ sql/connection.js
@@ -1,15 +1,25 @@
 const mysql = require('mysql')
+// const pool = require('./sql/connection');
+require('dotenv').config()
+
+const host = process.env.HOST
+const user = process.env.USER
+const password = process.env.PASSWORD
+const database = process.env.DATABASE
+
+
 
 class Connection {
   constructor() {
     if (!this.pool) {
       console.log('creating connection...')
+      console.log(host, user, password, database)
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
+        host: host,
+        user: user,
+        password: password,
+        database: database
       })
 
       return this.pool
